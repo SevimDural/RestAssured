f79c49346af415750a455d6457bd5f03fe1638c1
sevim <shdural82@gmail.com>
2021-08-24 22:03:32 +0200

    .body() islemleri

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,4 +1,5 @@
 
+import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -18,5 +19,62 @@ public class ZippoTest {
         ;
 
     }
+
+    @Test
+    public void statusCodeTest() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()  // log().all diyince tum respons u verecek. body diyince sadece body kkismindakileri
+                .statusCode(200)
+
+        ;
+    }
+
+    @Test
+    public void contentTypeTest() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                .contentType(ContentType.JSON)
+        ;
+
+    }
+
+    @Test
+    public void logAllTest() {
+        given()
+                .log().all()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+
+        ;
+
+    }
+
+    @Test
+    public void checkStateInResponseBody() {
+        given()
+                .when()
+                .get("http://api.zippopotam.us/us/90210")
+                .then()
+                .log().body()
+                //.body("country", equalTo("United States"))  // body.country==United States
+
+                //.body("places[0].state", equalTo("California")) // altaki ile ayni
+                //.statusCode(200)
+                .body("places.'state'", hasItem("California"));
+
+
+
+        ;
+
+    }
+
 
 }
