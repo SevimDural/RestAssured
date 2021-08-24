14abc1ce34c1ce9c3df0dc6a0180efb7e536e875
sevim <shdural82@gmail.com>
2021-08-24 20:32:21 +0200

    hallo

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,22 @@
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+    @Test
+    public void test() {
+        given()
+                // hazirlik islemlerini yapiyoruz
+                .when()
+                // link ve aksiyon islmeleri
+                .then()
+
+        // test ve extrackt islemleri
+
+        ;
+
+    }
+
+}
